00e22b74ebcf70e5b45ed2ddff5b60e5452cc098
Adam Funk <35650608+funkaj@users.noreply.github.com>
2018-06-25 14:12:25 -0700

    Revert "reworked photo calls per location"

--- assets/javascript/gPlaces.js
+++ assets/javascript/gPlaces.js
@@ -1,26 +1,35 @@
 let baseURL = 'https://maps.googleapis.com/maps/api/place/textsearch/json?';
 let keyAPI = '&key=AIzaSyAtAoE-_wQqtKgBC2vDZ_l4Y0qnpzBaMfw';
-let locQuery = '';
+let locQuery = 'query=';
 let photoMaxWidth = 'maxwidth=400';
-let photoURL = 'https://maps.googleapis.com/maps/api/place/photo?' // base photoURL. Needs the photoReference and maxWidth before it works
+let photoURL = 'https://maps.googleapis.com/maps/api/place/photo?'
+let photoReference = '&photoreference=';
 
-$('form').submit(function (event) {
-    event.preventDefault()
-    let photoReference = '&photoreference='; // this is obtained from the main places search
-    locQuery = 'query=' + $('#location').val();
-    queryURL = baseURL + locQuery + keyAPI; // First places request based on search textbox
+let queryURL = baseURL + keyAPI;
+$( document ).ready(function() {
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function (response) {
+      })
+      .then(function(response) {
+        //   console.log(response);
+      });
+})
+
+$('form').submit(function(event) {
+    event.preventDefault()
+    locQuery += $('#location').val();
+    queryURL = baseURL + locQuery + keyAPI;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
         console.log(response.results);
         photoReference += response.results[0].photos[0].photo_reference;
-        console.log(`Photo URL for AJAX: ${photoURL + photoMaxWidth + photoReference + keyAPI}`);
-        if (response.results[0].photos.length > 0) {
-            $('#photoDIV').append(`<img src="${photoURL + photoMaxWidth + photoReference + keyAPI}">`)
-        } else {
-            console.log(`No photos to show`)
-        }
-
+        console.log(`PhotoReference: ${photoReference}`)
+        console.log(`Photo URL for AJAX: ${photoURL + photoMaxWidth + photoReference + keyAPI}`)
+    }).then(function() {
+        $('#photoDIV').append(`<img src="${photoURL + photoMaxWidth + photoReference + keyAPI}>`)
+        
     })
 })
