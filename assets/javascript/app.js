ac81e466052e03c7c74ee5c8c50c0586d2c1079e
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-06-27 11:24:30 -0700

    added loading gif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,4 @@
-// start progress bar
-$(document).ready(function(){
 
-    let progress = setInterval(function() {
-        let $bar = $('.progress-bar');
-
-        if ($bar.width()>=400) {
-            clearInterval(progress)
-            $('.progress-bar').removeClass('active')
-        } else {
-            $bar.width($bar.width()+40)
-        }
-        
-    }, 1000)
-})
-// end progress bar
 
 
 
