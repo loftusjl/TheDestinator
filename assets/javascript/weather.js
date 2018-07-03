9635b21f1f8ac698c1f9c7aa5f03306c5bbb33fa
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-07-02 21:17:39 -0700

    updated weather.js day picking

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,3 +1,4 @@
+let dayArray = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
 function weatherForcast(searchCity) {
 
   var APIKey = "87231f56cfbb4322a1a44bc975de93ac";
@@ -11,39 +12,46 @@ function weatherForcast(searchCity) {
     .then(function (weatherForecast) {
       let city = weatherForecast.city.name;
       let dailyForecast = weatherForecast.list;
-      console.log(city)
-      console.log('here is the array ', dailyForecast)
-   
+      //console.log(city)
+      //console.log('here is the array ', dailyForecast)
+
+      let temperatureArray = []
+      
       for (i = 0; i < dailyForecast.length; i++) {
-        let hour = moment(dailyForecast[i].dt_txt).format('MM-DD-YYYY HH:mm')
+        let day = moment(dailyForecast[i].dt_txt).format('ddd')
+        let hour = moment(dailyForecast[i].dt_txt).format('HH')
+        temperatureArray.push({
+          day: day,
+          tempMax: dailyForecast[i].main.temp_max,
+          tempMin: dailyForecast[i].main.temp_min
+        })
+        
         let temp = dailyForecast[i].main.temp
         let wind = dailyForecast[i].wind.speed
         let humidity = dailyForecast[i].main.humidity
         let icon = $('.day1').html('<img src=http://openweathermap.org/img/w/' + dailyForecast[0].weather[0].icon + '.png>')
-        let day = dailyForecast[i].dt_txt
-        //console.log(temp, wind, humidity)
-        //console.log(hour)
-        $(".day1").text("Forecast: " + day  + " " + icon + " " + temp + " " + humidity + " " + wind);
-        $(".day2").text("Forecast: " + day + " " + temp + " " + humidity + " " + wind);
-        $(".day3").text("Forecast: " + day + " " + temp + " " + humidity + " " + wind);
-        $(".day4").text("Forecast: " + day + " " + temp + " " + humidity + " " + wind);
-        $(".day5").text("Forecast: " + day + " " + temp + " " + humidity + " " + wind);
-      }
+        
+        
+          //console.log(temp, wind, humidity)
+          //console.log(hour)
+          $(".day1").text(day + " " + icon + " " + temp + " " + humidity + " " + wind);
+          $(".day2").text(day + " " + icon + " " + temp + " " + humidity + " " + wind);
+          $(".day3").text(day + " " + icon + " " + temp + " " + humidity + " " + wind);
+          $(".day4").text(day + " " + icon + " " + temp + " " + humidity + " " + wind);
+          $(".day5").text(day + " " + icon + " " + temp + " " + humidity + " " + wind);
+        }
+         //temperatureArray.filter(dayFilter(i))
+         //console.log('Temperature Array', temperatureArray.filter(dayFilter(i)))
+      //console.log(temperatureArray)
+      for (d = 0; d < dayArray.length; d++) {
+      //Math.max(temperatureArray.filter(tempMax))
+      //Math.min(temperatureArray.filter(tempMin))
+      //console.log(Math.max(temperatureArray.filter(tempMax)))
+    }
     })
 
 
-  //   for (var i = 0; i < hour.length; i++) {
-  //   if (hour === 00) {
-  //     day++
-  //   } else {
-  // $(".city").html("<h1>" + response.city.name + " Weather Details</h1>");
-  // $(".icon").html('<img src=http://openweathermap.org/img/w/' + response.list[i].weather[0].icon + '.png>');
-  // $(".weather").text("Forecast: " + response.list[i].weather[0].main);
-  // $(".humidity").text("Humidity: " + response.list[i].main.humidity);
-  // $(".temp").text("Temperature (F) " + response.list[i].main.temp);
-  // }
 
 
-  // }
 
 }
