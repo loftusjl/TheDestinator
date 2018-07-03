b993ec54ad080a2e565f5d78b85efad77b48beca
JesseL <35356853+loftusjl@users.noreply.github.com>
2018-07-02 20:12:53 -0700

    added a couple options for dropdown point of interest

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,9 @@
-// let searchCity = $('#autocomplete').val();
-
+// Dropdown Points of Interest
+$('ul.nav li.dropdown').hover(function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+  }, function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+  });
 // start progress bar
 function loadingOn() {
     document.getElementById("loading").style.display = "block";
